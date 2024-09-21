6d08f312dc1decfe2d1da1b07b59b66a039a69ab
Nicolai Hähnle <nhaehnle@gmail.com>
2024-09-21 23:36:43 +0200

    Project dirs, app structure

--- git-review-tui/src/bin/git-review-tui.rs
+++ git-review-tui/src/bin/git-review-tui.rs
@@ -1,33 +1,69 @@
 use std::io;
 
 use ratatui::{
+    buffer::Buffer,
     crossterm::event::{self, KeyCode, KeyEventKind},
+    layout::Rect,
     style::Stylize,
-    widgets::Paragraph,
+    widgets::{Paragraph, Widget},
     DefaultTerminal,
 };
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
+use directories::ProjectDirs;
 
+#[derive(Debug)]
+struct App {
+    project_dirs: ProjectDirs,
+    exit: bool,
+}
+
+impl App {
+    pub fn init() -> io::Result<Self> {
+        let project_dirs = ProjectDirs::from("", "", "git-review-tui").unwrap();
+
+        std::fs::create_dir_all(&project_dirs.config_dir())?;
+        std::fs::create_dir_all(&project_dirs.cache_dir())?;
+
+        Ok(Self {
+            project_dirs,
+            exit: false,
+        })
+    }
+
+    pub fn run(&mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| frame.render_widget(&*self, frame.area()))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
         if let event::Event::Key(key) = event::read()? {
             if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
+                self.exit = true;
             }
         }
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+            .white()
+            .on_blue()
+            .render(area, buf);
     }
 }
 
 fn main() -> io::Result<()> {
+    let mut app = App::init()?;
+
     let mut terminal = ratatui::init();
     terminal.clear()?;
-    let app_result = run(terminal);
+    let app_result = app.run(terminal);
     ratatui::restore();
+
     app_result
 }
